a8148ec4907a9c710fdcd0daaecbf180b18780bc
Idris Rampurawala <idris.rampurawala@hotmail.com>
2020-05-02 10:37:13 +0530

    initial setup commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+import { HighlightDirective } from './directives/highlight.directive';
+import { FilterPipe } from './pipes/filter.pipe';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighlightDirective,
+    FilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
